c0cd55138ea3ef16c94ec79c98242b6f6b3ca42c
Necryotiks <eman13vil@gmail.com>
2017-05-10 22:50:30 -0700

    GOT CSV TO WORK

--- KRUW_Website/KRUW_Website/KRUW_Website.py
+++ KRUW_Website/KRUW_Website/KRUW_Website.py
@@ -5,10 +5,17 @@ import importlib
 import csv
 
 test_record = Record() #initalize Record object
-test_record.name = "Elliott"  #set name
-
-with open("Record_List.csv",'a+') as FileOBJ:
+test_record.name = "Elliott Villars"  #set name
+test_record.phone_num = "360-473-3137"
+test_record.email = "eman13vil@gmail.com"
+test_record.address = "9260 Ramiller LN SE"
+test_record.city = "Port Orchard"
+test_record.state = "WA"
+test_record.zip = "98367"
+test_record.carrier = "AT&T"
+test_record.text = "THIS IS A TEST RECORD"
+with open("Record_List.csv",'w+') as FileOBJ:
         CSVWriter = csv.writer(FileOBJ,delimiter = ',') # currently seperates each letter(FIXED)
-        CSVWriter.writerow([test_record.name]);
+        CSVWriter.writerow((test_record.name,test_record.phone_num,test_record.email,test_record.address,test_record.city,test_record.state,test_record.zip,test_record.carrier,test_record.text)) #need to figure out how to write the whole record
 
 
--- KRUW_Website/KRUW_Website/Records.py
+++ KRUW_Website/KRUW_Website/Records.py
@@ -3,6 +3,9 @@ class Record:
     phone_num = None
     email = None
     address = None
+    city = None
+    state = None
+    zip = None
     carrier = None
     text = None
 def __init__(self):#Initialize the object; functions like a constructor 
@@ -10,6 +13,9 @@ def __init__(self):#Initialize the object; functions like a constructor
     self.phone_num = ""
     self.email = ""
     self.address = ""
+    self.city = ""
+    self.state = ""
+    self.zip = ""
     self.carrier = ""
     self.text = ""
 def add_Name(self,name): #First,Last name
@@ -18,8 +24,14 @@ def add_Phone(self,phone_num):#phone number
 		self.phone_num = phone_num
 def add_Email(self,email):#email address
 		self.email = email
-def add_Address(self,address):#physical address
+def add_Street_Address(self,address):#physical street address
 		self.address = address
+def add_City(self,city_name):
+    self.city = city_name
+def add_State(self,state):
+    self.state = state
+def add_ZIP(self,zip_num):
+    self.zip = zip_num
 def add_Carrier(self,carrier):#phone carrier
 		self.carrier = carrier
 def add_Textblock(self,text):#text for record
