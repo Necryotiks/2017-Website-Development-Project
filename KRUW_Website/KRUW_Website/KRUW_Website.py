6613667c07643a06dfe92e9d1bb55c22aa0b84eb
Necryotiks <eman13vil@gmail.com>
2017-05-21 16:19:49 -0700

    PRE-ALPHA

--- KRUW_Website/KRUW_Website/KRUW_Website.py
+++ KRUW_Website/KRUW_Website/KRUW_Website.py
@@ -4,10 +4,13 @@ from Records import Record
 import threading
 import importlib
 from _Reader import recordReader
+from _Writer import recordWriter
 
 recordArray = []
 
 recordArray = recordReader(recordArray) #Reads form file. stores into record
+
+recordWriter(recordArray)#BROKE AS FUCK
 
 #with open("Record_List.csv",'a') as FileOBJ:
 #    CSVWriter = csv.writer(FileOBJ)
