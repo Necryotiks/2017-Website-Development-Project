f83d679f87bd2d6fdfea605b53358d5f278b886d
Necryotiks <eman13vil@gmail.com>
2017-05-21 22:15:26 -0700

    WRITING BROKE

--- KRUW_Website/KRUW_Website/_Writer.py
+++ KRUW_Website/KRUW_Website/_Writer.py
@@ -1,9 +1,9 @@
 import csv
-from Records import Record
+#from Records import Record
 
 def recordWriter(recordArray):
     with open("Record_List.csv",'w') as FileOBJ:
         CSVWriter = csv.writer(FileOBJ)
-        for line in recordArray:
+        for line in recordArray: #BROKE
             CSVWriter.writerow((line.name,line.phone_num,line.email,line.address,line.city,line.state,line.zip,line.carrier,line.text))#finish
         return
