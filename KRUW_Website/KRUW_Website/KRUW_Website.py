547d9af2d0bc45a44c65190baf270c01a7184dff
Necryotiks <eman13vil@gmail.com>
2017-05-27 16:49:09 -0700

    PRE ALPHA: AWAITNG MODELIZATION

--- KRUW_Website/KRUW_Website/KRUW_Website.py
+++ KRUW_Website/KRUW_Website/KRUW_Website.py
@@ -1,6 +1,5 @@
 import _io
 import fileinput
-from Records import Record
 import threading
 import importlib
 from _Reader import recordReader
@@ -9,8 +8,8 @@ from _Writer import recordWriter
 recordArray = []
 
 recordArray = recordReader(recordArray) #Reads form file. stores into record
-
-recordWriter(recordArray)#BROKE AS FUCK
+#
+#recordWriter(recordArray)#BROKE AS FUCK
 
 #with open("Record_List.csv",'a') as FileOBJ:
 #    CSVWriter = csv.writer(FileOBJ)
