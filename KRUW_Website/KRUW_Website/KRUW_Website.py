d07ce008d809c2e8a9d358fe376dcfd8a87e0c94
Necryotiks <eman13vil@gmail.com>
2017-05-28 15:15:26 -0700

    BURNED THIS MOTHERFUCKER DOWN

--- KRUW_Website/KRUW_Website/KRUW_Website.py
+++ KRUW_Website/KRUW_Website/KRUW_Website.py
@@ -1,18 +1,18 @@
-import _io
-import fileinput
-import threading
-import importlib
-from _Reader import recordReader
-from _Writer import recordWriter
+#import _io
+#import fileinput
+#import threading
+#import importlib
+#from _Reader import recordReader
+#from _Writer import recordWriter
 
-recordArray = []
+#recordArray = []
 
-recordArray = recordReader(recordArray) #Reads form file. stores into record
-#
-#recordWriter(recordArray)#BROKE AS FUCK
+#recordArray = recordReader(recordArray) #Reads form file. stores into record
+##
+##recordWriter(recordArray)#BROKE AS FUCK
 
-#with open("Record_List.csv",'a') as FileOBJ:
-#    CSVWriter = csv.writer(FileOBJ)
-#    for row in recordArray:
+##with open("Record_List.csv",'a') as FileOBJ:
+##    CSVWriter = csv.writer(FileOBJ)
+##    for row in recordArray:
        
 
